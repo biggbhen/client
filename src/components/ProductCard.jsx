5053ac7fee4e2a284baef673e3b14f048c4bde9f
Ugochukwu Benjamin <benstar531@gmail.com>
2023-12-28 05:55:12 +0100

    update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import foodImg from '../assets/food-image.svg';
 import thumbsUp from '../assets/thumbs-up.svg';
+import add from '../assets/add_food.svg';
 
 const ProductCard = ({ item }) => {
 	return (
 		<div
 			key={item._id}
-			className=' border-[.5px] border-[#D9DCE2] flex justify-between p-[1rem]  items-center rounded-[.4rem] hover:shadow-lg cursor-pointer'>
+			className=' border-[.5px] border-[#D9DCE2] flex justify-between p-[1rem]  items-center rounded-[.4rem] hover:shadow-lg '>
 			<div className=' w-[12.5rem]'>
 				<h4 className=' font-bold '>{item.title}</h4>
 				<p className='text-[0.87rem] text-[#858D9D]'>{item.description}</p>
@@ -21,17 +21,22 @@ const ProductCard = ({ item }) => {
 					</span>
 					<span className='text-[#EE2024] line-through text-[0.87rem] font-semibold '>
 						{' '}
-						{item.salesPrice}
+						{`N${item.salesPrice}`}
 					</span>
 					<span className='text-[0.87rem] text-[#2B2F38] font-semibold '>
-						{item.defaultPrice}
+						{`N${item.defaultPrice}`}
 					</span>
 				</div>
 			</div>
-			<div>
+			<div className='relative'>
 				<img
 					src={item.productImage}
 					className='w-[90px] h-[90px] rounded-[8px]'
+					alt='foodImg'
+				/>
+				<img
+					src={add}
+					className='w-[30px] h-[30px] absolute translate-x-[50%] right-[50%] bottom-[-15px] cursor-pointer'
 					alt='foodImg'
 				/>
 			</div>
